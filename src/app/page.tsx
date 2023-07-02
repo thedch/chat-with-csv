7bf3b6943e5c32ecb41af13c4b0388948f59373c
Daniel <13357734+thedch@users.noreply.github.com>
2023-07-02 10:09:52 -0700

    Generaly seems to work

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,109 @@
-import Image from 'next/image'
+'use client'
 
-export default function Home() {
+import { useState } from 'react'
+import { useChat } from 'ai/react'
+
+function CSVUploader() {
+  const [csvData, setCsvData] = useState<string[][]>([])
+  const [isFileUploaded, setIsFileUploaded] = useState(false);
+
+  const handleFileUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0]
+    if (file) {
+      const reader = new FileReader()
+      reader.onload = (e) => {
+        const csvText = e.target?.result as string
+        const rows = csvText.split('\n').map(row => row.split(','))
+        setCsvData(rows)
+        setIsFileUploaded(true)  // file upload successful, change state
+      }
+      reader.readAsText(file)
+    }
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+    <div>
+      <div className="flex justify-end">
+      <label className="flex items-center p-1 bg-blue-500 hover:bg-blue-600 text-white rounded cursor-pointer">
+        <span className="mr-2">{isFileUploaded ? "Try another file" : "Upload file"}</span>
+        <input 
+          type="file" 
+          accept=".csv" 
+          onChange={handleFileUpload}
+          className="hidden"  // hide default input style
         />
+      </label>
       </div>
+      <table style={{ 
+        borderCollapse: 'collapse',
+        border: '1px solid black',
+        width: '100%',
+        maxWidth: '800px',
+        margin: '0 auto',
+        fontSize: '14px',
+        fontFamily: 'Arial, sans-serif',
+        backgroundColor: '#f9f9f9'
+      }}>
+        <thead>
+          <tr>
+            {csvData[0]?.map((cell, cellIndex) => (
+              <th key={cellIndex} style={{ 
+                border: '1px solid black',
+                padding: '8px'
+              }}>{cell}</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {csvData.slice(1).map((row, rowIndex) => (
+            <tr key={rowIndex}>
+              {row.map((cell, cellIndex) => (
+                <td key={cellIndex} style={{ 
+                  border: '1px solid black',
+                  padding: '8px'
+                }}>{cell}</td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  )
+}
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+export default function Chat() {
+  const { messages, input, handleInputChange, handleSubmit } = useChat( {id: 'daniel-test'} )
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+  return (
+    <>
+      <div className="flex flex-col min-h-screen">
+        <div className="flex-grow overflow-y-scroll">
+          <CSVUploader />
+          <div className="px-4 py-2">
+            {messages.map(m => (
+              <div key={m.id} className="flex items-center">
+                <div className={`rounded py-2 px-3 mr-2 ${m.role === 'user' ? 'bg-blue-100' : 'bg-gray-100'}`}>
+                  {m.role}
+                </div>
+                <div className='p-10'>{m.content}</div>
+              </div>
+            ))}
+          </div>
+        </div>
+        <form onSubmit={handleSubmit} className="flex items-center px-4 py-2 border-t border-gray-200">
+          <input
+            value={input}
+            onChange={handleInputChange}
+            className="flex-1 px-2 py-1 mr-2 border border-gray-200 rounded focus:outline-none focus:border-blue-200"
+          />
+          <button
+            type="submit"
+            className="px-3 py-1 bg-blue-500 text-white rounded hover:bg-blue-600"
+          >
+            Send, length of messages is {messages.length}
+          </button>
+        </form>
       </div>
-    </main>
+    </>
   )
 }
