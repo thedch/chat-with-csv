933410f865e8203a91277c66f53d660c14df6a32
Daniel <13357734+thedch@users.noreply.github.com>
2023-07-02 21:12:21 -0700

    Files reinflate now

--- src/app/History.tsx
+++ src/app/History.tsx
@@ -17,22 +17,35 @@ function HistoryPage() {
 
 
   return (
-    <div>
-      <h1>Session History</h1>
-      <button onClick={handleNewSessionClick}>New Session</button>
-      <ul>
-        {sessions.map((session: any) => (
-          <li key={session.id} onClick={() => handleSessionClick(session.id)}>
-            Session {session.id.substring(0, 8)}...
-            <br />
-            Filename: {session.name}
-            <br />
-            Number of messages: {session.messages.length}
-          </li>
-        ))}
-      </ul>
+    <div className="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
+      <div className="relative py-3 sm:max-w-xl sm:mx-auto">
+        <div className="absolute inset-0 bg-gradient-to-r from-cyan-400 to-light-blue-500 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
+        <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
+          <h1 className="text-2xl font-semibold text-gray-900">Session History</h1>
+          <button
+            onClick={handleNewSessionClick}
+            className="mt-4 px-4 py-2 bg-blue-600 text-white text-base font-semibold rounded-lg shadow-md hover:bg-blue-700"
+          >
+            New Session
+          </button>
+          <ul className="mt-6 space-y-4">
+            {sessions.map((session: any) => (
+              <li
+                key={session.id}
+                onClick={() => handleSessionClick(session.id)}
+                className="px-4 py-2 rounded-lg shadow-md hover:shadow-lg cursor-pointer bg-gray-100"
+              >
+                <div className="text-lg font-semibold text-gray-900">Session {session.id.substring(0, 8)}...</div>
+                <div className="text-sm font-medium text-gray-600">Filename: {session.name}</div>
+                <div className="text-sm font-medium text-gray-600">Number of messages: {session.messages.length}</div>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </div>
   );
+
 }
 
 export default HistoryPage;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom';
 import { useChat } from 'ai/react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { v4 as uuidv4 } from 'uuid'; // for generating unique session IDs
@@ -9,8 +10,7 @@ import { useNavigate } from 'react-router-dom';
 
 
 
-// function CSVUploader() {
-function CSVUploader({ setSessionId }: { setSessionId: (id: string) => void }) {
+function CSVUploader({ setSessionId, sessionId }: { setSessionId: (id: string) => void, sessionId: string | null }) {
   const [csvData, setCsvData] = useState<string[][]>([])
   const [isFileUploaded, setIsFileUploaded] = useState(false);
 
@@ -24,13 +24,13 @@ function CSVUploader({ setSessionId }: { setSessionId: (id: string) => void }) {
         setCsvData(rows)
         setIsFileUploaded(true)  // file upload successful, change state
 
-        const sessionId = uuidv4(); // generate a unique ID for this session
-        setSessionId(sessionId); // notify the Chat component of the new session ID
+        const newSessionId = uuidv4(); // generate a unique ID for this session
+        setSessionId(newSessionId); // notify the Chat component of the new session ID
 
         // Save to local storage
         const sessions = JSON.parse(localStorage.getItem('sessions') || '[]')
         sessions.push({
-          id: sessionId,
+          id: newSessionId,
           name: file.name,
           data: rows,
           messages: [], // start with no messages for this session
@@ -41,20 +41,32 @@ function CSVUploader({ setSessionId }: { setSessionId: (id: string) => void }) {
     }
   }
 
+  // Load file from local storage if a sessionId is passed in
+  useEffect(() => {
+    if (sessionId) {
+      const sessions = JSON.parse(localStorage.getItem('sessions') || '[]')
+      const currentSession = sessions.find(session => session.id === sessionId)
+      if (currentSession) {
+        setCsvData(currentSession.data)
+        setIsFileUploaded(true)
+      }
+    }
+  }, [sessionId])
+
   return (
     <div>
       <div className="flex justify-end">
       <label className="flex items-center p-1 bg-blue-500 hover:bg-blue-600 text-white rounded cursor-pointer">
         <span className="mr-2">{isFileUploaded ? "Try another file" : "Upload file"}</span>
-        <input 
-          type="file" 
-          accept=".csv" 
+        <input
+          type="file"
+          accept=".csv"
           onChange={handleFileUpload}
           className="hidden"  // hide default input style
         />
       </label>
       </div>
-      <table style={{ 
+      <table style={{
         borderCollapse: 'collapse',
         border: '1px solid black',
         width: '100%',
@@ -67,7 +79,7 @@ function CSVUploader({ setSessionId }: { setSessionId: (id: string) => void }) {
         <thead>
           <tr>
             {csvData[0]?.map((cell, cellIndex) => (
-              <th key={cellIndex} style={{ 
+              <th key={cellIndex} style={{
                 border: '1px solid black',
                 padding: '8px'
               }}>{cell}</th>
@@ -78,7 +90,7 @@ function CSVUploader({ setSessionId }: { setSessionId: (id: string) => void }) {
           {csvData.slice(1).map((row, rowIndex) => (
             <tr key={rowIndex}>
               {row.map((cell, cellIndex) => (
-                <td key={cellIndex} style={{ 
+                <td key={cellIndex} style={{
                   border: '1px solid black',
                   padding: '8px'
                 }}>{cell}</td>
@@ -92,8 +104,20 @@ function CSVUploader({ setSessionId }: { setSessionId: (id: string) => void }) {
 }
 
 function Chat() {
-  const { messages, input, handleInputChange, handleSubmit } = useChat( {id: 'daniel-test'} )
-  const [sessionId, setSessionId] = useState<string | null>(null);
+  const { sessionId } = useParams(); // this gets the sessionId from the route
+  const [localSessionId, setLocalSessionId] = useState<string | null>(sessionId);
+
+  // Load pre-existing messages from local storage
+  let initialMessages = [];
+  if (sessionId) {
+    const sessions = JSON.parse(localStorage.getItem('sessions') || '[]');
+    const currentSession = sessions.find(session => session.id === sessionId);
+    if (currentSession) {
+      initialMessages = currentSession.messages;
+    }
+  }
+  // const { messages, input, handleInputChange, handleSubmit } = useChat( {id: 'daniel-test'} )
+  const { messages, input, handleInputChange, handleSubmit } = useChat( {id: 'daniel-test', initialMessages } )
 
   const navigate = useNavigate();
 
@@ -101,18 +125,16 @@ function Chat() {
     navigate('/');
   };
 
-
-  // Save messages to localStorage every time they change
   useEffect(() => {
-    if (sessionId) { // only save messages if there is a current session
+    if (localSessionId) {
       const sessions = JSON.parse(localStorage.getItem('sessions') || '[]')
-      const currentSession = sessions.find(session => session.id === sessionId)
+      const currentSession = sessions.find(session => session.id === localSessionId)
       if (currentSession) {
         currentSession.messages = messages
         localStorage.setItem('sessions', JSON.stringify(sessions))
       }
     }
-  }, [messages, sessionId])
+  }, [messages, localSessionId])
 
 
   return (
@@ -120,7 +142,7 @@ function Chat() {
       <div className="flex flex-col min-h-screen">
         <div className="flex-grow overflow-y-scroll">
           <button onClick={handleBackClick}>Back to History</button>
-          <CSVUploader setSessionId={setSessionId} />
+          <CSVUploader setSessionId={setLocalSessionId} sessionId={sessionId}/>
           <div className="px-4 py-2">
             {messages.map(m => (
               <div key={m.id} className="flex items-center">
