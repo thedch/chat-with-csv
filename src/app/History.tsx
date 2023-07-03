82b2cdaf946990ae9affc51a882bdbbe534a5f33
Daniel <13357734+thedch@users.noreply.github.com>
2023-07-02 21:57:43 -0700

    Big UI wins, messages clear now

--- src/app/History.tsx
+++ src/app/History.tsx
@@ -15,7 +15,6 @@ function HistoryPage() {
     navigate(`/chat/${newSessionId}`);
   };
 
-
   return (
     <div className="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
       <div className="relative py-3 sm:max-w-xl sm:mx-auto">
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,12 +9,14 @@ import HistoryPage from './History'
 import { useNavigate } from 'react-router-dom';
 
 
-
 function CSVUploader({ setSessionId, sessionId }: { setSessionId: (id: string) => void, sessionId: string | null }) {
   const [csvData, setCsvData] = useState<string[][]>([])
   const [isFileUploaded, setIsFileUploaded] = useState(false);
 
+  const navigate = useNavigate();
+
   const handleFileUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
+    console.log('!!! Got new file upload event', event.target.files)
     const file = event.target.files?.[0]
     if (file) {
       const reader = new FileReader()
@@ -25,6 +27,7 @@ function CSVUploader({ setSessionId, sessionId }: { setSessionId: (id: string) =
         setIsFileUploaded(true)  // file upload successful, change state
 
         const newSessionId = uuidv4(); // generate a unique ID for this session
+        console.log('!!! New session ID', newSessionId)
         setSessionId(newSessionId); // notify the Chat component of the new session ID
 
         // Save to local storage
@@ -36,6 +39,7 @@ function CSVUploader({ setSessionId, sessionId }: { setSessionId: (id: string) =
           messages: [], // start with no messages for this session
         })
         localStorage.setItem('sessions', JSON.stringify(sessions))
+        navigate(`/chat/${newSessionId}`);
       }
       reader.readAsText(file)
     }
@@ -53,52 +57,49 @@ function CSVUploader({ setSessionId, sessionId }: { setSessionId: (id: string) =
     }
   }, [sessionId])
 
+  const handleBackClick = () => {
+    navigate('/');
+  };
+
   return (
-    <div>
-      <div className="flex justify-end">
-      <label className="flex items-center p-1 bg-blue-500 hover:bg-blue-600 text-white rounded cursor-pointer">
-        <span className="mr-2">{isFileUploaded ? "Try another file" : "Upload file"}</span>
-        <input
-          type="file"
-          accept=".csv"
-          onChange={handleFileUpload}
-          className="hidden"  // hide default input style
-        />
-      </label>
+    <div className="mx-auto max-w-full p-6 bg-white shadow rounded">
+      <div className="flex justify-between mb-4">
+        <button
+          onClick={handleBackClick}
+          className="px-4 py-2 text-white bg-blue-500 rounded hover:bg-blue-600 transition-colors"
+        >
+          Back to History
+        </button>
+        <label className="flex items-center px-4 py-2 text-white bg-blue-500 rounded cursor-pointer hover:bg-blue-600">
+          <span className="mr-2">{isFileUploaded ? "Try another file" : "Upload file"}</span>
+          <input
+            type="file"
+            accept=".csv"
+            onChange={handleFileUpload}
+            className="hidden"  // hide default input style
+          />
+        </label>
       </div>
-      <table style={{
-        borderCollapse: 'collapse',
-        border: '1px solid black',
-        width: '100%',
-        maxWidth: '800px',
-        margin: '0 auto',
-        fontSize: '14px',
-        fontFamily: 'Arial, sans-serif',
-        backgroundColor: '#f9f9f9'
-      }}>
-        <thead>
-          <tr>
-            {csvData[0]?.map((cell, cellIndex) => (
-              <th key={cellIndex} style={{
-                border: '1px solid black',
-                padding: '8px'
-              }}>{cell}</th>
-            ))}
-          </tr>
-        </thead>
-        <tbody>
-          {csvData.slice(1).map((row, rowIndex) => (
-            <tr key={rowIndex}>
-              {row.map((cell, cellIndex) => (
-                <td key={cellIndex} style={{
-                  border: '1px solid black',
-                  padding: '8px'
-                }}>{cell}</td>
+      <div className="overflow-x-auto max-h-[30vh] overflow-y-auto">
+        <table className="w-full text-left table-auto border-collapse">
+          <thead>
+            <tr>
+              {csvData[0]?.map((cell, cellIndex) => (
+                <th key={cellIndex} className="px-4 py-2 text-sm font-medium text-gray-700 bg-gray-100 border border-gray-200">{cell}</th>
               ))}
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody className="text-sm text-gray-700">
+            {csvData.slice(1).map((row, rowIndex) => (
+              <tr key={rowIndex} className={`${rowIndex % 2 === 0 ? 'bg-gray-50' : ''}`}>
+                {row.map((cell, cellIndex) => (
+                  <td key={cellIndex} className="px-4 py-2 border border-gray-200">{cell}</td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   )
 }
@@ -116,16 +117,10 @@ function Chat() {
       initialMessages = currentSession.messages;
     }
   }
-  // const { messages, input, handleInputChange, handleSubmit } = useChat( {id: 'daniel-test'} )
   const { messages, input, handleInputChange, handleSubmit } = useChat( {id: 'daniel-test', initialMessages } )
 
-  const navigate = useNavigate();
-
-  const handleBackClick = () => {
-    navigate('/');
-  };
-
   useEffect(() => {
+    // console.log('!!! New Messages', localSessionId)
     if (localSessionId) {
       const sessions = JSON.parse(localStorage.getItem('sessions') || '[]')
       const currentSession = sessions.find(session => session.id === localSessionId)
@@ -134,37 +129,37 @@ function Chat() {
         localStorage.setItem('sessions', JSON.stringify(sessions))
       }
     }
-  }, [messages, localSessionId])
-
+  }, [messages])
 
   return (
     <>
-      <div className="flex flex-col min-h-screen">
+      <div className="flex flex-col min-h-screen bg-gray-50">
         <div className="flex-grow overflow-y-scroll">
-          <button onClick={handleBackClick}>Back to History</button>
-          <CSVUploader setSessionId={setLocalSessionId} sessionId={sessionId}/>
-          <div className="px-4 py-2">
+          <header className="flex items-center justify-between p-6 bg-white border-b-2 border-gray-200">
+              <CSVUploader setSessionId={setLocalSessionId} sessionId={sessionId}/>
+          </header>
+          <main className="p-6">
             {messages.map(m => (
-              <div key={m.id} className="flex items-center">
-                <div className={`rounded py-2 px-3 mr-2 ${m.role === 'user' ? 'bg-blue-100' : 'bg-gray-100'}`}>
+              <div key={m.id} className="flex items-start mb-4">
+                <div className={`rounded py-2 px-3 mr-2 text-sm font-medium ${m.role === 'user' ? 'bg-blue-100 text-blue-700' : 'bg-gray-100 text-gray-700'}`}>
                   {m.role}
                 </div>
-                <div className='p-10'>{m.content}</div>
+                <div className='px-4 py-2 bg-white rounded shadow'>{m.content}</div>
               </div>
             ))}
-          </div>
+          </main>
         </div>
-        <form onSubmit={handleSubmit} className="flex items-center px-4 py-2 border-t border-gray-200">
+        <form onSubmit={handleSubmit} className="flex items-center p-6 bg-white border-t-2 border-gray-200">
           <input
             value={input}
             onChange={handleInputChange}
-            className="flex-1 px-2 py-1 mr-2 border border-gray-200 rounded focus:outline-none focus:border-blue-200"
+            className="flex-grow px-4 py-2 mr-4 bg-gray-100 rounded focus:outline-none focus:bg-white border border-transparent focus:border-blue-200 transition-colors"
           />
           <button
             type="submit"
-            className="px-3 py-1 bg-blue-500 text-white rounded hover:bg-blue-600"
+            className="px-6 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition-colors"
           >
-            Send, length of messages is {messages.length}
+            Send ({localSessionId})
           </button>
         </form>
       </div>
